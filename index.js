0ad1f01114c998406c3ea78b61eca36a54b324d5
Ashraful Asif <ashrafulasif260@gmail.com>
2023-09-16 20:56:29 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", function(e){
+    if(e.target.classList.contains("add")){
+        count.innerHTML++;
+    };
+
+    if(e.target.classList.contains("subtract")){
+        count.innerHTML--;
+    };
+
+    if(e.target.classList.contains("reset")){
+        count.innerHTML=0;
+    };
+});
