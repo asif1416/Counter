86df65ac0e5c6cad2cc14ffd9c01d8122f70b529
Ashraful Asif <ashrafulasif260@gmail.com>
2023-10-05 10:35:54 +0600

    This is a counter app
    Dark mode added

--- index.js
+++ index.js
@@ -14,3 +14,28 @@ buttons.addEventListener("click", function(e){
         count.innerHTML=0;
     };
 });
+
+
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const body = document.body;
+const nav = document.querySelector(".nav"); // Select the .nav element
+
+// Check if the user's preference is already set
+if (localStorage.getItem("darkMode") === "enabled") {
+  body.classList.add("dark-mode");
+  nav.classList.add("dark-mode"); // Apply dark mode to the .nav element
+}
+
+darkModeToggle.addEventListener("click", () => {
+  // Toggle dark mode for body and .nav
+  body.classList.toggle("dark-mode");
+  nav.classList.toggle("dark-mode"); // Toggle dark mode for .nav as well
+
+  // Save user's preference to localStorage
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
+});
+
